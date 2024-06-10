27a91303cd406f50f91703b01ae4974b8d1a57ca
kblizzy22 <kbailey322@gmail.com>
2024-06-10 09:05:02 -0400

    Edited index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ function generateRecipe(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey= "91o0f2f84c841e4a7c38t44e6d3be32a";
-  let prompt = 'Ingredient: Generate a recipe containing ${instructionsInput.value}';
-  let context = "You are an expert chef of global cuisine. Create a recipe that revolves around the ingredient entered by the user.";
-  
-  let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+  let apiKey = "91o0f2f84c841e4a7c38t44e6d3be32a";
+  let context =
+    "You are an expert chef of global cuisine. Create a recipe that revolves around the ingredient entered by the user.";
+  let prompt = `Ingredient: Generate a recipe containing ${instructionsInput.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating recipe...");
 
